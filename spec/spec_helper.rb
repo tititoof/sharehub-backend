8daff7cfa85f1d7affe851e4e10ea321fa6bc255
Christophe Hartmann <chartmann.35@gmail.com>
2023-05-13 10:49:26 +0200

    fix(ci-cd) : Add simplecov for SonarQube

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,13 @@
 # it.
 #
 # See https://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+require 'simplecov'
+require 'simplecov-json'
+
+SimpleCov.start 'rails' do
+  SimpleCov.formatter = SimpleCov::Formatter::JSONFormatter
+end
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
