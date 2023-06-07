ac1697b39884454dbaa8a6088e093e74f8a5daa3
Christophe Hartmann <chartmann.35@gmail.com>
2023-06-07 08:37:16 +0200

    fix(quality) : Rubocop check

--- app/models/concerns/code_qualities/qualitable.rb
+++ app/models/concerns/code_qualities/qualitable.rb
@@ -1,18 +1,17 @@
 # frozen_string_literal: true
 
-# The Qualitable module is a behavior module that can be included in an ActiveRecord model.
-# It extends the model with a polymorphic association "has_many :albums, as: :qua" that allows a model
-# to be associated with multiple albums. The polymorphic key "as: :albumable" allows different models to share
-# the same database table for albums, storing both the parent model's ID and type.
-# The "dependent: :destroy" option ensures that all albums associated with this parent model are
-# deleted from the database when the parent model is destroyed.
 module CodeQualities
+  # The Qualitable module is a behavior module that can be included in an ActiveRecord model.
+  # It extends the model with a polymorphic association "has_many :albums, as: :qua" that allows a model
+  # to be associated with multiple albums. The polymorphic key "as: :albumable" allows different models to share
+  # the same database table for albums, storing both the parent model's ID and type.
+  # The "dependent: :destroy" option ensures that all albums associated with this parent model are
+  # deleted from the database when the parent model is destroyed.
   module Qualitable
     extend ActiveSupport::Concern
-  
+
     included do
       has_many :managements, as: :qualitable, class_name: '::CodeQualities::Management', dependent: :destroy
     end
   end
 end
-  
