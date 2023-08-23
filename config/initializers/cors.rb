b64bd4216707d7f03082f440279b448762cff982
Christophe Hartmann <chartmann.35@gmail.com>
2023-08-23 09:36:19 +0200

    test(ci-cd): setup jenkins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -16,7 +16,7 @@
 # end
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://sharehub-frontend.traefik.me'
+    origins <%= Rails.application.credentials.origins %>
 
     resource(
       '*',
